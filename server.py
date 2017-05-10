ddba24b96bb69038bf1213576a2413226c32e9ac
Cristian Gociu <cristi@devicehub.net>
2017-05-10 16:02:08 +0300

    Work with parameters

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ __author__ = 'cristi'
 from datetime import datetime
 
 import twitter
-from flask import Flask
+from flask import Flask, request
 
 
 
@@ -12,14 +12,18 @@ ACCESS_TOKEN = '862241031940603904-UK9htq79xVWYwMtn89vNw6UHAgbQVcI'
 ACCESS_TOKEN_SECRET = 'AQrAqWAp4MTnPJtb7JqEG94xMfnCRViPX8vWbnmJmPgGo'
 
 
-def detect_earthquake():
+def detect_earthquake(geocode=None, keyword=None):
+    if not geocode:
+        geocode = '44.413731,26.158461,150km'
+    if not keyword:
+        keyword = 'cutremur'
     api = twitter.Api(consumer_key=CONSUMER_KEY,
                       consumer_secret=CONSUMER_SECRET,
                       access_token_key=ACCESS_TOKEN,
                       access_token_secret=ACCESS_TOKEN_SECRET)
     today = datetime.now()
-    query = "q=cutremur%20&result_type=recent&since={}-{}-{}&count=100&geocode=44.413731,26.158461,150km&lang=ro".format(
-        today.year, today.month, today.day)
+    query = "q={}%20&result_type=recent&since={}-{}-{}&count=100&geocode={}&lang=ro".format(
+        keyword, today.year, today.month, today.day, geocode)
     results = api.GetSearch(raw_query=query)
     return results
 
@@ -27,8 +31,10 @@ def detect_earthquake():
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return str(len(detect_earthquake()))
+def bridge():
+    geocode = request.args.get('g')
+    keyword = request.args.get('w')
+    return str(len(detect_earthquake(geocode, keyword)))
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8013)
